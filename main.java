45a7464ba9eca4c652a765e81264cb8b0cc7f788
elkarim <32342242+elkarim@users.noreply.github.com>
2017-11-09 10:26:11 +0100

    1ere version
    Cinema

---
+++ main.java
@@ -0,0 +1,16 @@
+
+public class main {
+
+	public static void main(String[] args) {
+		{
+			Cinema film1=new film(starwars);//le film projete dans la salle,
+			try {
+				film1.nbPlaces=1000;  //la capacit´e de la salle,
+			    film1.tarif=7.5; //le prix d’un billet d’entr´ee.
+			} catch (Exception e) {  system.out.println(e.getMessage(); }  
+			film1.tosting();
+			
+		
+		}
+
+}
